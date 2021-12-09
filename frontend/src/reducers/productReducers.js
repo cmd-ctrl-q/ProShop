2ef052e73819abcae3b23f4652367f841ad20795
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-12-09 12:20:14 -0600

    add redux state to homescreen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,7 +2,7 @@ import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL} from '..
 // handles state for product list 
 // actions dispatches an action to the productListReducer
 // the initial state for products is an empty array
-export const productListReducer = (state = { product: [] }, action) => {
+export const productListReducer = (state = { products: [] }, action) => {
     switch(action.type) {
         case PRODUCT_LIST_REQUEST:
             // request is currently loading
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,48 @@
 import React, { useState, useEffect } from 'react'
+// useDispatch dispatches actions
+// useSelector selects parts of the state, (eg productList)
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
 
-    const [products, setProducts] = useState([])
+    // create hook
+    const dispatch = useDispatch()
+
+    // get the productList from the state
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products} = productList
 
     useEffect(() => {
+        // make request to backend to get products using the dispatcher
+        dispatch(listProducts())
+    }, [dispatch])  // pass dispatch as a dependency
 
-        // since axios returns a promise, you have to call await
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            setProducts(data)
-        }
-
-        fetchProducts()
-    }, []) 
+    // if (loading) {
+    //     return <p>Loading...</p>
+    // } else if (error) {
+    //     return <p>{error.message}</p>
+    // }
 
     return (
-        <div>
+        <>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map((product, i) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </div>
+            {loading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <h3>{error}</h3>
+            ) : (
+                <Row>
+                    {products.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+            )}
+        </>
     )
 }
 
