251a0b434dbbf29466ef448ce927eab80bd56614
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-12-13 23:52:26 -0600

    create user authentication endpoint; add matchedPassword method to userSchema, and call in user controller

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
     name: {
@@ -22,6 +23,13 @@ const userSchema = mongoose.Schema({
 }, {
     timestamps: true
 })
+
+// compares if user entered password matches the password in the database
+// this.password comes from matchPassword
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    // returns a promise so use await
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
