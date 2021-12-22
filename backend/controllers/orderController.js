7f815e1631a91d01b8e5032c3c2e089720e2a9fb
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-12-22 16:37:37 -0600

    create route and controller for getting order by id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -35,8 +35,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
         const createdOrder = await order.save()
         res.status(201).json(createdOrder)
     }
-
-
 })
 
-export { addOrderItems } 
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private 
+const getOrderById = asyncHandler(async (req, res) => {
+
+    // get from db 
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
+    if (order) {
+        res.json(order)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+export { addOrderItems, getOrderById } 
