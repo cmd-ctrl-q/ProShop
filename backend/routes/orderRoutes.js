d5af9ac469bb23a3692ca89bc2b2bc127cc47b42
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-12-21 19:05:10 -0600

    fix: import protect middleware

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 import {
     addOrderItems, 
 } from '../controllers/orderController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
 
