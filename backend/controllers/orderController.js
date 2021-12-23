79b74ddc75f989129489f465564ae79cadb6efd2
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-12-23 17:01:30 -0600

    create and update-to-paid controller and route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -52,4 +52,30 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
-export { addOrderItems, getOrderById } 
+// @desc    Update order to paid
+// @route   GET /api/orders/:id
+// @access  Private 
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+
+    // get from db 
+    const order = await Order.findById(req.params.id)
+    if (order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        // from paypal
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+        const updateOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid } 
