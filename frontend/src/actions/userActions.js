63eb397919d8a6a701f873b49630d62a974b2541
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-12-26 14:04:54 -0600

    create constants, reducer, and action for getting list of all users

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -14,6 +14,9 @@ import {
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
 } from "../constants/userConstants"
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
@@ -171,6 +174,40 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: USER_UPDATE_PROFILE_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        }) 
+    }
+}
+
+
+export const listUsers = () => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_LIST_REQUEST
+        })
+
+        // get access to logged in users info
+        // get userInfo which is in userLogin
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        // make put request to backend, to update user profile
+        const { data } = await axios.get(`/api/users`, config)
+
+        // dispatch register success
+        dispatch({
+            type: USER_LIST_SUCCESS,
+            payload: data,
+        })
+
+    } catch (error) {
+        dispatch({
+            type: USER_LIST_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         }) 
     }
