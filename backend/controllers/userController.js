2d537562532d8127aa8b9689d756d455074f4b81
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-12-28 14:54:56 -0600

    create admin routes and controllers for getting user by id and updateing user by id

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -130,6 +130,44 @@ const deleteUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    GET user by ID 
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+    if (user) {
+        res.json(user)
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        user.isAdmin = req.body.isAdmin === undefined ? user.isAdmin : req.body.isAdmin
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id, 
+            name: updatedUser.name, 
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin, 
+        })
+    } else {
+        res.status(404) // user not found 
+        throw new Error('User not found')
+    }
+})
+
 export {
     authUser, 
     getUserProfile,
@@ -137,4 +175,6 @@ export {
     updateUserProfile,
     getUsers,
     deleteUser,
+    getUserById,
+    updateUser,
 }
