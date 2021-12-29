17af2af96b5334bc6a6ac4ad25dbcc15fe8eb8d5
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-12-29 13:37:08 -0600

    create route and handler for deleting a product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,11 +23,27 @@ const getProductById = asyncHandler(async (req, res) => {
         res.status(404) 
         throw new Error('Product not found')
     }
+})
 
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin 
+const deleteProduct = asyncHandler(async (req, res) => {
 
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product removed' })
+    } else {
+        // res.status(404).json({ message: 'Product not found' })
+        res.status(404) 
+        throw new Error('Product not found')
+    }
 })
 
 export {
     getProducts,
-    getProductById
+    getProductById,
+    deleteProduct,
 }
