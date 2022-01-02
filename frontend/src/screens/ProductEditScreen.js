a31eaf046b8e43fad368e0d1b48de367f00ba1f2
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-01-01 21:57:13 -0600

    add dispatch for updating product and dispay product being updated in a form

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -24,25 +24,45 @@ const ProductEditScreen = () => {
     const productDetails = useSelector(state => state.productDetails)
     const { loading, error, product } = productDetails
 
+    const productUpdate = useSelector(state => state.productUpdate)
+    const { 
+        loading: loadingUpdate, 
+        error: errorUpdate, 
+        success: successUpdate,
+    } = productUpdate
+
     useEffect(() => {
-        if (!product.name || product._id !== productId) {
-            dispatch(listProductDetails(productId))
-            // navigate(-1) 
+        if (successUpdate) {
+            dispatch({ type: PRODUCT_UPDATE_RESET })
+            navigate('/admin/productlist')
         } else {
-            setName(product.name)
-            setPrice(product.price)
-            setImage(product.image)
-            setBrand(product.brand)
-            setCategory(product.category)
-            setCountInStock(product.countInStock)
-            setDescription(product.description)
+            if (!product.name || product._id !== productId) {
+                dispatch(listProductDetails(productId))
+            } else {
+                setName(product.name)
+                setPrice(product.price)
+                setImage(product.image)
+                setBrand(product.brand)
+                setCategory(product.category)
+                setCountInStock(product.countInStock)
+                setDescription(product.description)
+            }
         }
-    }, [dispatch, product, productId])
+    }, [dispatch, product, productId, navigate, successUpdate])
 
     const submitHandler = (e) => {
         // prevent page from refreshing
         e.preventDefault()
-        // UPDATE PRODUCT
+        dispatch(updateProduct({
+            _id: productId, 
+            name, 
+            price,
+            image, 
+            brand, 
+            category,
+            description,
+            countInStock,
+        }))
     }
 
     return (
@@ -53,6 +73,8 @@ const ProductEditScreen = () => {
 
             <FormContainer>
                 <h1>Edit Product</h1>
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
                 {loading 
                     ? <Loader /> 
                     : error 
