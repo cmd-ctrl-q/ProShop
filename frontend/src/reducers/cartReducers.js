753f81fd7209d311d0e0b981a50f25f855325bb2
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-01-04 22:12:27 -0600

    create constants and reducer for listing all orders

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -3,6 +3,7 @@ import {
     CART_REMOVE_ITEM,
     CART_SAVE_SHIPPING_ADDRESS,
     CART_SAVE_PAYMENT_METHOD,
+    CART_CLEAR_ITEMS,
  } from '../constants/cartConstants'
 
 // state is the initial state
@@ -48,6 +49,11 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, acti
                     ...state, 
                     // action.payload is the data from the form
                     paymentMethod: action.payload,
+                }
+            case CART_CLEAR_ITEMS:
+                return {
+                    ...state, 
+                    cartItems: [],
                 }
         default:
             return state
