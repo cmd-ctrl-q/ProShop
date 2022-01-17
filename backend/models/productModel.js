52c8a24569eae343e8a7edac7f685dc49495bd7c
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-01-16 22:32:35 -0600

    create controller and endpoint for creating a review

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -5,6 +5,11 @@ const reviewSchema = mongoose.Schema({
     // one review rating
     rating: { type: Number, required: true },
     comment: { type: String, required: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
 }, {
     timestamps: true
 })
