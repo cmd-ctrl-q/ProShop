2af957c2504e6feb9a823db98725f6fd490a9607
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-01-20 15:18:11 -0600

    prep for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,12 +21,7 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
-// accept json in body
 app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -40,6 +35,17 @@ app.get('/api/config/paypal', (req, res) =>
 // make uploads folder static for loading in web browser
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
+
 
 app.use(notFound)
 app.use(errorHandler)
